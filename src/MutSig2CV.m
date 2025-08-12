31462613cafcbbf00f70a767bbe4b7025a103960
David Lehotzky <63481084+Lehotzky@users.noreply.github.com>
2025-08-12 12:52:34 -0400

    Update reference folder name

--- src/MutSig2CV.m
+++ src/MutSig2CV.m
@@ -21,15 +21,15 @@ end
 if ~exist('P','var'), P=[]; end
 
 P = impose_default_value(P,'mutation_blacklist_file',        'none');
-P = impose_default_value(P,'mutation_type_dictionary_file',  'reference/mutation_type_dictionary.v4.txt');
-P = impose_default_value(P,'coverage_models_mat_file',       'reference/coverage_models.agilent.mat');
+P = impose_default_value(P,'mutation_type_dictionary_file',  'MutSig2CV_reference/mutation_type_dictionary.v4.txt');
+P = impose_default_value(P,'coverage_models_mat_file',       'MutSig2CV_reference/coverage_models.agilent.mat');
 P = impose_default_value(P,'basewise_coverage_fwb_file',     'none');
-P = impose_default_value(P,'target_list_file',               'reference/target_list.gencode25.txt');
-P = impose_default_value(P,'context_and_effect_fwb_file',    'reference/context_and_effect_track.gencodeV39.fwb');
-P = impose_default_value(P,'context_and_effect_categs_file', 'reference/context_and_effect.c65e29b.txt');
-P = impose_default_value(P,'covariates_file',                'reference/covariates.agilent.txt');
-P = impose_default_value(P,'conservation_fwb_file',          'reference/conservation_phyloP100way_hg38.fwb');
-P = impose_default_value(P,'FixedWidthBinary_jar_file',      'reference/FixedWidthBinary.jar');
+P = impose_default_value(P,'target_list_file',               'MutSig2CV_reference/target_list.gencode25.txt');
+P = impose_default_value(P,'context_and_effect_fwb_file',    'MutSig2CV_reference/context_and_effect_track.gencodeV39.fwb');
+P = impose_default_value(P,'context_and_effect_categs_file', 'MutSig2CV_reference/context_and_effect.c65e29b.txt');
+P = impose_default_value(P,'covariates_file',                'MutSig2CV_reference/covariates.agilent.txt');
+P = impose_default_value(P,'conservation_fwb_file',          'MutSig2CV_reference/conservation_phyloP100way_hg38.fwb');
+P = impose_default_value(P,'FixedWidthBinary_jar_file',      'MutSig2CV_reference/FixedWidthBinary.jar');
 
 P = impose_default_value(P,'build','hg38');
 P = impose_default_value(P,'impute_full_coverage', true);
